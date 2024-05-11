29f4fe89a02148b3cd653ec0bdfcd8873db4abe7
iutest <haiyang@gmail.com>
2024-05-11 11:35:04 -0400

    Add files via upload

---
+++ s.py
@@ -0,0 +1,11 @@
+import numpy as np
+from itertools import product
+#from random import shuffle
+import random
+
+import time
+import turns
+from turns import Cube
+
+start_time = time.time()
+print(start_time)
